0fa64a7728b7f76cd97827f5919883699699d364
林谦 <linqian@linewell.com>
2021-12-02 17:51:35 +0800

    源码解析

--- spring-lq/src/main/java/com/lq/circulardependency/Main.java
+++ spring-lq/src/main/java/com/lq/circulardependency/Main.java
@@ -2,6 +2,8 @@ package com.lq.circulardependency;
 
 import com.lq.circulardependency.entity.A;
 import com.lq.circulardependency.entity.B;
+import com.lq.circulardependency.entity.Person;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -14,11 +16,14 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
 
 	public static void main(String[] args) {
-		AbstractApplicationContext ac = new ClassPathXmlApplicationContext("applicationContext.xml");
-		//AbstractApplicationContext bd = new MyClassPathXmlApplication("applicationContext-${username}.xml");
+//		AbstractApplicationContext ac = new ClassPathXmlApplicationContext("applicationContext.xml");
+		AbstractApplicationContext bd = new ClassPathXmlApplicationContext("applicationContext-${username}.xml");
+		Person p = bd.getBean(Person.class);
+		ApplicationContext ac = p.getAc();
+		System.out.println(ac);
 
-		A a = ac.getBean(A.class);
-		B b = ac.getBean(B.class);
+//		A a = bd.getBean(A.class);
+//		B b = bd.getBean(B.class);
 	}
 
 }
--- spring-lq/src/main/java/com/lq/circulardependency/entity/Person.java
+++ spring-lq/src/main/java/com/lq/circulardependency/entity/Person.java
@@ -1,14 +1,19 @@
 package com.lq.circulardependency.entity;
 
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
 /**
  * @program: spring
  * @description:
  * @author: linqian
  * @create: 2021-07-06 15:08
  **/
-public class Person {
+public class Person implements ApplicationContextAware {
 	private String name;
 	private int age;
+	private ApplicationContext ac;
 
 	public String getName() {
 		return name;
@@ -24,5 +29,15 @@ public class Person {
 
 	public void setAge(int age) {
 		this.age = age;
+	}
+
+	public ApplicationContext getAc() {
+		return ac;
+	}
+
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		System.out.println("applicationContext==============");
+		this.ac = applicationContext;
 	}
 }
